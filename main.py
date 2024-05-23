025fdcf324cc33b4b467d5420e7874d94aafbda1
ivantaiwan <94451659+ivantaiwan@users.noreply.github.com>
2024-05-23 17:55:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 from linebot.v3 import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
-from linebot.v3.webhooks import MessageEvent, TextMessageContent, JoinEvent, UserSource,
+from linebot.v3.webhooks import MessageEvent, TextMessageContent, JoinEvent, UserSource
 from linebot.v3.webhooks import MemberJoinedEvent
 
 app = Flask(__name__)
