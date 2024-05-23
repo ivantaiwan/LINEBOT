2f082346c9bdea74c7f33bd511277070bd150b2d
ivantaiwan <94451659+ivantaiwan@users.noreply.github.com>
2024-05-23 17:30:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -46,19 +46,44 @@ def handle_message(event):
 
 
 
+# @handler.add(MemberJoinedEvent)
+# def handle_member_joined(event):
+#     joined_user_ids = [member.user_id for member in event.joined.members]
+#     welcome_message = "Hello!!!"
+#     welcome_message = f"""歡迎 {', '.join(joined_user_ids)} 加入群組，目前記事本有最新球敘相關訊息，可以先去看看喔，如果要報名的話，直接在底下喊+1，然後在@主揪喔，另外有一些小提醒也是要看一下喔，個人簡介也麻煩填一下，開心擊球，無壓力唷😁😁
+# 啊如果有開團，可以@Astor，我會幫您丟到記事本唷😁😁"""
+
+#     with ApiClient(configuration) as api_client:
+#         line_bot_api = MessagingApi(api_client)
+#         line_bot_api.reply_message_with_http_info(ReplyMessageRequest(
+#             reply_token=event.reply_token,
+#             messages=[TextMessage(text=welcome_message)]
+#         ))
+
 @handler.add(MemberJoinedEvent)
 def handle_member_joined(event):
+    api_client = ApiClient(configuration)
+    line_bot_api = MessagingApi(api_client)
+    
     joined_user_ids = [member.user_id for member in event.joined.members]
+    names = []
+    
+    for user_id in joined_user_ids:
+        try:
+            profile = line_bot_api.get_profile(user_id)
+            names.append(profile.display_name)  # 使用顯示名稱
+        except LineBotApiError as e:
+            app.logger.error(f"Failed to get profile for user {user_id}: {e}")
+            names.append("未知用戶")  # 無法獲得資料的預設處理
     welcome_message = "Hello!!!"
     welcome_message = f"""歡迎 {', '.join(joined_user_ids)} 加入群組，目前記事本有最新球敘相關訊息，可以先去看看喔，如果要報名的話，直接在底下喊+1，然後在@主揪喔，另外有一些小提醒也是要看一下喔，個人簡介也麻煩填一下，開心擊球，無壓力唷😁😁
-啊如果有開團，可以@Astor，我會幫您丟到記事本唷😁😁"""
+# 啊如果有開團，可以@Astor，我會幫您丟到記事本唷😁😁"""
 
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(ReplyMessageRequest(
-            reply_token=event.reply_token,
-            messages=[TextMessage(text=welcome_message)]
-        ))
+    line_bot_api.reply_message_with_http_info(ReplyMessageRequest(
+        reply_token=event.reply_token,
+        messages=[TextMessage(text=welcome_message)]
+    ))
+
 
 
 if __name__ == "__main__":
