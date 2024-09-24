15c3340735c360d151b459e34d641e80a4a5349d
Yung-Chen Tang <kumitaiwan@gmail.com>
2024-09-24 15:46:50 +0800

    update golf price info layout v2.4

--- main.py
+++ main.py
@@ -137,15 +137,9 @@ def handle_message(event):
             thumbnail_image_url=FIG_GOLF_COURSE,
             text='請選擇想查詢的地區！',
             actions=[
-                MessageAction(label='雙北', text='Hello!'),
-                MessageAction(label='桃園', text='Hello!'),
-                MessageAction(label='竹苗', text='Hello!'),
-                MessageAction(label='中部', text='Hello!'),
-                MessageAction(label='南部', text='Hello!'),
-                MessageAction(label='東部', text='Hello!'),
-                # URIAction(label='雙北', uri=TABLE_TAIPEI),
-                # URIAction(label='桃園', uri=TABLE_TAOYUAN),
-                # URIAction(label='竹苗', uri=TABLE_HSINCHU),
+                URIAction(label='雙北', uri=TABLE_TAIPEI),
+                URIAction(label='桃園', uri=TABLE_TAOYUAN),
+                URIAction(label='竹苗', uri=TABLE_HSINCHU),
                 # URIAction(label='中部', uri=TABLE_CENTRAL),
                 # URIAction(label='南部', uri=TABLE_SOUTH),
                 # URIAction(label='東部', uri=TABLE_EAST)
