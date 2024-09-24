6e0e96cc7a4d2c6c0f283412c38966c3bc485a58
Yung-Chen Tang <kumitaiwan@gmail.com>
2024-09-24 15:41:24 +0800

    update golf price info layout v2.3

--- main.py
+++ main.py
@@ -137,7 +137,12 @@ def handle_message(event):
             thumbnail_image_url=FIG_GOLF_COURSE,
             text='請選擇想查詢的地區！',
             actions=[
-                MessageAction(label='說哈囉', text='Hello!'),
+                URIAction(label='雙北', uri='https://www.google.com'),
+                URIAction(label='桃園', uri='https://www.google.com'),
+                URIAction(label='竹苗', uri='https://www.google.com'),
+                URIAction(label='中部', uri='https://www.google.com'),
+                URIAction(label='南部', uri='https://www.google.com'),
+                URIAction(label='南部', uri='https://www.google.com')
                 # URIAction(label='雙北', uri=TABLE_TAIPEI),
                 # URIAction(label='桃園', uri=TABLE_TAOYUAN),
                 # URIAction(label='竹苗', uri=TABLE_HSINCHU),
