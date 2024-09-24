0d0fdf040af090dcc43f9e44d0ccb2d9935791fc
Yung-Chen Tang <kumitaiwan@gmail.com>
2024-09-24 17:18:40 +0800

    add config.py v1.2

--- config.py
+++ config.py
@@ -1,15 +1,18 @@
-WELCOME_MESSAGE = """歡迎加入群組！群組無任何版主，由熱心的球友們共同維護，要拉人請隨意，無需告知。\n\n請遵守板規如下⬇️\n\n1. 不做任何營銷及商業行為，若需生意交流，請私下與球友討論，不要構成其他人壓力。\n\n2. 不傳遞賣場連結或其他群組連結（預防球友遭受詐騙或其他不預期事件）\n\n3. 球隊要拉人也請在球敘後與球友討論，切勿在版內公開拉人，不造成其他人壓力。\n\n❤️目前記事本有最新球敘相關訊息，可以先去看看喔！如果要報名的話，直接在底下喊+1，然後再@主揪喔。\n\n另外有一些小提醒也是要看一下喔，個人簡介也麻煩填一下，開心擊球，無壓力唷😁😁\n\n如果要開團，可以放到記事本讓大家加入唷😁😁 \n\n球場價格: https://sites.google.com/view/tw-golf-price"""
+MESSAGE = {
+    'WELCOME': """歡迎加入群組！群組無任何版主，由熱心的球友們共同維護，要拉人請隨意，無需告知。\n\n請遵守板規如下⬇️\n\n1. 不做任何營銷及商業行為，若需生意交流，請私下與球友討論，不要構成其他人壓力。\n\n2. 不傳遞賣場連結或其他群組連結（預防球友遭受詐騙或其他不預期事件）\n\n3. 球隊要拉人也請在球敘後與球友討論，切勿在版內公開拉人，不造成其他人壓力。\n\n❤️目前記事本有最新球敘相關訊息，可以先去看看喔！如果要報名的話，直接在底下喊+1，然後再@主揪喔。\n\n另外有一些小提醒也是要看一下喔，個人簡介也麻煩填一下，開心擊球，無壓力唷😁😁\n\n如果要開團，可以放到記事本讓大家加入唷😁😁 \n\n球場價格: https://sites.google.com/view/tw-golf-price""",
+    'HELLO': """HI, 我是群組專用的 AI!"""
+}
 
 TABLE = {
-    'taipei' = 'https://docs.google.com/spreadsheets/d/1urXlSqNkxKm67KwnN85HWFcxO93MX90M1WZ58Pnzqc8/edit?gid=0#gid=0',
-    'taoyuan_hsinchu' = 'https://docs.google.com/spreadsheets/d/1fEX6-BmrxsBqfrpYx58QT01eEG6k1TJgKWLEFxwJ4z4/edit?gid=0#gid=0',
-    'other' = 'https://docs.google.com/spreadsheets/d/1HEGYhE0PfVOwFqn5Avp01KSkP0UUXEUURx0MheIgJz0/edit?gid=0#gid=0'
+    'taipei': 'https://docs.google.com/spreadsheets/d/1urXlSqNkxKm67KwnN85HWFcxO93MX90M1WZ58Pnzqc8/edit?gid=0#gid=0',
+    'taoyuan_hsinchu': 'https://docs.google.com/spreadsheets/d/1fEX6-BmrxsBqfrpYx58QT01eEG6k1TJgKWLEFxwJ4z4/edit?gid=0#gid=0',
+    'other': 'https://docs.google.com/spreadsheets/d/1HEGYhE0PfVOwFqn5Avp01KSkP0UUXEUURx0MheIgJz0/edit?gid=0#gid=0'
 }
 
 FIGURE = {
-    'golf_course' = 'https://raw.githubusercontent.com/ivantaiwan/LINEBOT/refs/heads/main/image/golf_course.jpg',
-    'golf_game' = 'https://raw.githubusercontent.com/ivantaiwan/LINEBOT/refs/heads/main/image/golf_game.jpg',
-    'golf_icon' = 'https://raw.githubusercontent.com/ivantaiwan/LINEBOT/refs/heads/main/image/golf_icon.png',
-    'karina' = 'https://raw.githubusercontent.com/ivantaiwan/LINEBOT/refs/heads/main/image/karina.jpg',
-    'winter' = 'https://raw.githubusercontent.com/ivantaiwan/LINEBOT/refs/heads/main/image/winter.jpeg'
+    'golf_course': 'https://raw.githubusercontent.com/ivantaiwan/LINEBOT/refs/heads/main/image/golf_course.jpg',
+    'golf_game': 'https://raw.githubusercontent.com/ivantaiwan/LINEBOT/refs/heads/main/image/golf_game.jpg',
+    'golf_icon': 'https://raw.githubusercontent.com/ivantaiwan/LINEBOT/refs/heads/main/image/golf_icon.png',
+    'karina': 'https://raw.githubusercontent.com/ivantaiwan/LINEBOT/refs/heads/main/image/karina.jpg',
+    'winter': 'https://raw.githubusercontent.com/ivantaiwan/LINEBOT/refs/heads/main/image/winter.jpeg'
 }
