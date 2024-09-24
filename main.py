cdc41549aecd9fc6672e3e6f6b9657073081ede1
Yung-Chen Tang <kumitaiwan@gmail.com>
2024-09-24 17:26:33 +0800

    add config.py v1.3

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from linebot.models import (
     URIAction, MessageAction, QuickReply, QuickReplyButton)
 from linebot.models.template import ButtonsTemplate
 
-import config
+import config  # config file for all project
+
 
 app = Flask(__name__)
 
@@ -19,23 +20,10 @@ LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-# PARAMS
-WELCOME_MESSAGE = """歡迎加入群組！群組無任何版主，由熱心的球友們共同維護，要拉人請隨意，無需告知。\n\n請遵守板規如下⬇️\n\n1. 不做任何營銷及商業行為，若需生意交流，請私下與球友討論，不要構成其他人壓力。\n\n2. 不傳遞賣場連結或其他群組連結（預防球友遭受詐騙或其他不預期事件）\n\n3. 球隊要拉人也請在球敘後與球友討論，切勿在版內公開拉人，不造成其他人壓力。\n\n❤️目前記事本有最新球敘相關訊息，可以先去看看喔！如果要報名的話，直接在底下喊+1，然後再@主揪喔。\n\n另外有一些小提醒也是要看一下喔，個人簡介也麻煩填一下，開心擊球，無壓力唷😁😁\n\n如果要開團，可以放到記事本讓大家加入唷😁😁 \n\n球場價格: https://sites.google.com/view/tw-golf-price"""
-FIG_GOLF_COURSE = 'https://raw.githubusercontent.com/ivantaiwan/LINEBOT/refs/heads/main/image/golf_course.jpg'
-FIG_GOLF_GAME = 'https://raw.githubusercontent.com/ivantaiwan/LINEBOT/refs/heads/main/image/golf_game.jpg'
-FIG_GOLF_ICON = 'https://raw.githubusercontent.com/ivantaiwan/LINEBOT/refs/heads/main/image/golf_icon.png'
-FIG_KARINA = 'https://raw.githubusercontent.com/ivantaiwan/LINEBOT/refs/heads/main/image/karina.jpg'
-FIG_WINTER = 'https://raw.githubusercontent.com/ivantaiwan/LINEBOT/refs/heads/main/image/winter.jpeg'
-
-TABLE_TAIPEI = 'https://docs.google.com/spreadsheets/d/1urXlSqNkxKm67KwnN85HWFcxO93MX90M1WZ58Pnzqc8/edit?gid=0#gid=0'
-TABLE_TAOYUAN_HSINCHU = 'https://docs.google.com/spreadsheets/d/1fEX6-BmrxsBqfrpYx58QT01eEG6k1TJgKWLEFxwJ4z4/edit?gid=0#gid=0'
-TABLE_OTHER = 'https://docs.google.com/spreadsheets/d/1HEGYhE0PfVOwFqn5Avp01KSkP0UUXEUURx0MheIgJz0/edit?gid=0#gid=0'
-
 
 @app.route('/')
 def hello_world():
     return "Welcome to the service!"
-
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -59,7 +47,7 @@ def callback():
 def handle_member_joined(event):
         line_bot_api.reply_message(
             event.reply_token,
-            TextMessage(text=WELCOME_MESSAGE)
+            TextMessage(text=config.MESSAGE['welcome'])
         )
 
 @handler.add(MessageEvent, message=TextMessage)
@@ -88,15 +76,15 @@ def handle_message(event):
             event.reply_token,
             TemplateSendMessage(alt_text='Winter', template=image_carousel_template))
 
-    elif msg == '!球場價格':  # Display All Golf Course Pricing Information for Taiwan
+    elif msg in ['!球場價格', '球場價格!', '！球場價格', '球場價格！']:  # Display All Golf Course Pricing Information for Taiwan
         buttons_template = ButtonsTemplate(
             title='全台球場價格',
-            thumbnail_image_url=FIG_GOLF_COURSE,
+            thumbnail_image_url=config.FIGURE['golf_course'],
             text='請選擇想查詢的地區！',
             actions=[
-                URIAction(label='雙北', uri=TABLE_TAIPEI),
-                URIAction(label='桃竹苗', uri=TABLE_TAOYUAN_HSINCHU),
-                URIAction(label='中部南部東部', uri=TABLE_OTHER)
+                URIAction(label='雙北', uri=config.TABLE['taipei']),
+                URIAction(label='桃竹苗', uri=config.TABLE['taoyuan_hsinchu']),
+                URIAction(label='中部南部東部', uri=config.TABLE['other'])
             ]
         )
 
@@ -104,10 +92,10 @@ def handle_message(event):
             event.reply_token,
             TemplateSendMessage(alt_text='球場價格', template=buttons_template))
 
-    elif msg == '!約下場':  # Display Dashboard for Create/Join Golf Game
+    elif msg in ['!約下場', '約下場!', '！約下場', '約下場！']:  # Display Dashboard for Create/Join Golf Game
         buttons_template = ButtonsTemplate(
             title='高爾夫約下場',
-            thumbnail_image_url=FIG_GOLF_GAME,
+            thumbnail_image_url=config.FIGURE['golf_game'],
             text='請選擇！',
             actions=[
                 MessageAction(label='創建與編輯球局', text='Coming Soon!'),
